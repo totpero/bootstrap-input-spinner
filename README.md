835cf60fb18c91d40130aadb3cdd34c568f9ad1c
TROJal.exe <contat@totpe.ro>
2014-04-23 20:50:03 +0300

    -add option 'color';
    -update radme and index..

--- README.md
+++ README.md
@@ -1,4 +1,28 @@
 bootstrap-input-spinner
 =======================
 
-Add loading spinner on twitter bootstrap inputs
+jQuery Plugin to add loading spinner on twitter bootstrap inputs
+
+## Demo
+http://www.totpe.ro/
+
+##How to use:
+Call the plugin on the element/elements that you want to add spinner inside.
+
+##Options:
+```javascript
+	$(selector).inputSpinner({
+		'opacity'	: 0.5,
+		'color'		: 'red',
+		'glyphicon'	: 'glyphicon-refresh'
+	});
+	$('elem').inputSpinner({
+		'opacity'	: 0.5,
+		'color'		: 'red',
+		'glyphicon'	: 'glyphicon-refresh'
+	});
+```
+
+## About
+Author & copyright (c) 2014	: 	[Iulian Alexe](http://www.totpe.ro)<br/>
+E-mail						: 	contact@totpe.ro
--- js/jquey.bootstrap-input-spinner.js
+++ js/jquey.bootstrap-input-spinner.js
@@ -1,61 +1,65 @@
 /**************************************************************************/
-/*******************************Input Text Spinar**************************/
+/*******************************Input Text Spinner**************************/
 /**************************************************************************/
+/**
+* @license jQuery Twitter Bootstrap input text spinner plugin v1.0.0 23/04/2014
+* http://www.totpe.ro
+*
+* Copyright (c) 2014, Iulian Alexe (contact@totpe.ro)
+**/
 
 //Use Exemple:
 
 //$(".form-control").inputSpinner({ //options
-//    'opacity': 0.5,
-//    'glyphicon': 'glyphicon-refresh'
+//		'opacity'	: 0.5,
+//		'color'		: 'red',
+//		'glyphicon'	: 'glyphicon-refresh'
 //});
 
 //OR:
 
 //$(".form-control").inputSpinner();
 (function($) {
-    var inputSpinnerFunc = function(element, options) {
-        this.$element = $(element);
+	var inputSpinnerFunc = function(element, options) {
+		this.$element = $(element);
 
-        if (!(this.$element.is("input[type=text]") || this.$element.is("input[type=password]"))) {
-            console.log("Element must bee input type text or password");
-            return false;
-        }
+		if (!(this.$element.is("input[type=text]") || this.$element.is("input[type=password]"))) {
+			console.log("Element must bee input type text or password");
+			return false;
+		}
 
-        this.options = $.extend(true, {}, $.fn.inputSpinner.defaults, options);
+		this.options = $.extend(true, {}, $.fn.inputSpinner.defaults, options);
+		if (this.$element.parents().hasClass("has-feedback")) {
+			this.$element
+				.next("span")
+				.remove();
+			this.$element
+				.parents()
+				.removeClass("has-feedback");
+		} else {
+			this.$element
+				.after($("<span></span>")
+					.addClass("glyphicon input-spinner form-control-feedback")
+					.addClass(this.options.glyphicon)
+					.css({
+						'opacity': this.options.opacity,
+						'color': this.options.color
+					})
+				);
+			this.$element
+				.parents()
+				.addClass("has-feedback");
+		}
+		return this;
+	};
+	$.fn.inputSpinner = function(options) {
+		return new inputSpinnerFunc(this, options);
+	};
 
-        if (this.$element.parents().hasClass("has-feedback")) {
-
-            this.$element
-                .next("span")
-                .remove();
-
-            this.$element
-                .parents()
-                .removeClass("has-feedback");
-        } else {
-            this.$element
-                .after($("<span></span>")
-                    .addClass("glyphicon input-spinner form-control-feedback text-muted")
-                    .addClass(this.options.glyphicon)
-                    .css({
-                        'opacity': this.options.opacity
-                    })
-                );
-
-            this.$element
-                .parents()
-                .addClass("has-feedback");
-        }
-
-        return this;
-    };
-    $.fn.inputSpinner = function(options) {
-        return new inputSpinnerFunc(this, options);
-    };
-
-    $.fn.inputSpinner.defaults = {
-        opacity: 0.7,
-        glyphicon: 'glyphicon-refresh',
-    };
+	$.fn.inputSpinner.defaults = {
+		opacity		: 0.7,
+		color		: '#999999',
+		glyphicon	: 'glyphicon-refresh',
+	};
 
 })(window.jQuery);
